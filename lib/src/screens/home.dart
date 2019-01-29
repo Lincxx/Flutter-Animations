50c2ff3cab0cd0be18d0c5b6b4125e30566bba0f
Lincxx <bossco28@gmail.com>
2019-01-29 07:05:56 -0500

    Cat is moving down on a gestureDection with onTap

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -6,16 +6,58 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  //just declaring a variable
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  //initialize variable
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this, //this refers to the current class
+    );
+
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn,
+      ),
+    );
+
+    //catController.forward();
+  }
+
+  onTap() {
+    catController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Animation!'),),
-      body: buildAnimation(),
-    );
+        appBar: AppBar(
+          title: Text('Animation!'),
+        ),
+        body: GestureDetector(
+          child: buildAnimation(),
+          onTap: onTap,
+        ));
   }
 
-  Widget buildAnimation(){
-    return Cat();
+  Widget buildAnimation() {
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Container(
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(),
+    );
   }
 }
